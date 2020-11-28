892ad9d55ffe7fe0bae8058579795813a677a4f3
beatarynkevic <beatainn@gmail.com>
2020-11-28 14:08:02 +0200

    Simple function

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Funkcija skirta sumuoti
+ * @param {number} a pirmasis skaicius
+ * @param {number} b antrasis skaicius
+ * @returns {number} grazina suma dvieju skaitmenu
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(2, 3));
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+console.log(multiplication(4, 5));
